1fe0f1da35c65bf5826f49f593f4e7e63091854b
Ben Guo <ben@venmo.com>
2014-01-08 23:04:55 -0500

    Better lpf

--- js/app/synth.js
+++ js/app/synth.js
@@ -9,9 +9,9 @@ function(T) {
       var VCO = T("sin", {freq:opts.freq});
 
       var cutoff = T("env", {table:[440, [440, 500], [1660, 250]]}).bang();
-      var VCF    = T("lpf", {cutoff:cutoff, Q:1}, VCO);
+      var VCF    = T("lpf", {cutoff:1079, Q:1}, VCO);
 
-      var EG  = T("adsr", {a:15, d:0, s:0.2, r:15, lv:0.1});
+      var EG  = T("adsr", {a:15, d:0, s:0.2, r:0, lv:0.1});
       var VCA = EG.append(VCF).bang();
 
       return VCA;
